87027e6b471fd42c6f5caabb39dc4d98f603a96d
Chris Breuer <chrisbreuer93@gmail.com>
2019-06-11 16:30:50 -0700

    wip

--- src/QuoteFactory.php
+++ src/QuoteFactory.php
@@ -6,18 +6,47 @@ use Illuminate\Support\Collection;
 
 class QuoteFactory
 {
-    public function getRandomQuote()
+    public function quotes()
     {
-        return Collection::make([
+        return [
             [
                 'quote' => 'When there is no desire, all things are at peace.',
-                'author' => 'Laozi',
-                'avatar' => 'image'
+                'by' => 'Laozi',
+                'avatar' => 'https://i.imgur.com/hcfQHVk.png'
             ], [
                 'quote' => 'Simplicity is the ultimate sophistication.',
-                'author' => 'Leonardo da Vinci',
-                'avatar' => 'image'
+                'by' => 'Leonardo da Vinci',
+                'avatar' => 'https://i.imgur.com/fk7VpK6.png'
+            ], [
+                'quote' => 'Don’t judge each day by the harvest you reap but by the seeds that you plant.',
+                'by' => 'Robert Louis Stevenson',
+                'avatar' => 'https://i.imgur.com/I9GMkBS.png'
+            ], [
+                'quote' => 'Write it on your heart that every day is the best day in the year.',
+                'by' => 'Ralph Waldo Emerson',
+                'avatar' => 'https://i.imgur.com/1v1U8O2.jpg'
+            ], [
+                'quote' => 'Every moment is a fresh beginning.',
+                'by' => 'T.S. Eliot',
+                'avatar' => 'https://i.imgur.com/AjK9HKF.png'
+            ], [
+                'quote' => 'Without His love I can do nothing, with His love there is nothing I cannot do.',
+                'by' => 'Unknown',
+                'avatar' => ''
+            ], [
+                'quote' => 'Everything you’ve ever wanted is on the other side of fear.',
+                'by' => 'George Addair',
+                'avatar' => 'https://i.imgur.com/0YdRYJm.png'
+            ], [
+                'quote' => 'Begin at the beginning... and go on till you come to the end: then stop.',
+                'by' => 'Lewis Carroll',
+                'avatar' => 'https://i.imgur.com/s4RfIHa.png'
             ],
-        ])->random();
+        ];
+    }
+
+    public function getRandomQuote()
+    {
+        return Collection::make($this->quotes())->random();
     }
 }
--- tests/QuoteFactoryTest.php
+++ tests/QuoteFactoryTest.php
@@ -6,22 +6,9 @@ use Cion\InspirationalQuotes\Facades\InspirationalQuote;
 
 class QuoteFactoryTest extends TestCase
 {
-    protected $quotes = [
-        [
-            'quote' => 'When there is no desire, all things are at peace.',
-            'author' => 'Laozi',
-            'avatar' => 'image'
-        ],
-        [
-            'quote' => 'Simplicity is the ultimate sophistication.',
-            'author' => 'Leonardo da Vinci',
-            'avatar' => 'image'
-        ]
-    ];
-
     /** @test */
     public function it_returns_a_random_quote()
     {
-        $this->assertContains(InspirationalQuote::getRandomQuote(), $this->quotes);
+        $this->assertContains(InspirationalQuote::getRandomQuote(), InspirationalQuote::quotes());
     }
 }
